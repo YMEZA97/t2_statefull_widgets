d68bff6a93c65dcf49e623cd6a2e2a911ae91144
YMEZA97 <yasalberto@hotmail.com>
2024-08-03 01:50:34 -0600

    T2 Main init - Update 4

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
+var total = "23fgfh";
+
+// List of items in our dropdown menu
+const List<String> items = <String>[
+  'Suma',
+  'Resta',
+  'Multiplicacion',
+  'Division'
+];
+
+String dropdownValue = items.first;
+
+TextEditingController n1 = TextEditingController();
+TextEditingController n2 = TextEditingController();
+GlobalKey<FormState> keyForm = GlobalKey();
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
-TextEditingController n1 = TextEditingController();
-TextEditingController n2 = TextEditingController();
-TextEditingController sumatoria = TextEditingController();
-TextEditingController total = TextEditingController();
-
-GlobalKey<FormState> keyForm = GlobalKey();
 
 class _HomePageState extends State<HomePage> {
   @override
@@ -24,7 +33,7 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: Container(
-          margin: const EdgeInsets.all(20.0),
+          margin: const EdgeInsets.all(30.0),
           child: Form(
             key: keyForm,
             child: formUI(),
@@ -43,30 +52,56 @@ class _HomePageState extends State<HomePage> {
 
 formItemsDesign(item) {
   return Padding(
-    padding: const EdgeInsets.symmetric(vertical: 7),
-    child: Card(child: ListTile(title: item)),
-  );
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: ListTile(title: item));
 }
 
 Widget formUI() {
   return Column(children: <Widget>[
     formItemsDesign(TextField(
       controller: n1,
+      textAlign: TextAlign.center,
       keyboardType: const TextInputType.numberWithOptions(decimal: true),
       decoration: const InputDecoration(
           border: OutlineInputBorder(), labelText: 'Primer Valor'),
     )),
     formItemsDesign(TextField(
       controller: n2,
+      textAlign: TextAlign.center,
       keyboardType: const TextInputType.numberWithOptions(decimal: true),
       decoration: const InputDecoration(
           border: OutlineInputBorder(), labelText: 'Segundo Valor'),
     )),
-    formItemsDesign(TextField(
-      controller: total,
-      keyboardType: const TextInputType.numberWithOptions(decimal: true),
-      decoration: const InputDecoration(
-          border: OutlineInputBorder(), labelText: 'Total'),
-    )),
+    formItemsDesign(
+      DropdownButton(
+        // Initial Value
+        value: dropdownValue,
+
+        // Down Arrow Icon
+        icon: const Icon(Icons.keyboard_arrow_down),
+
+        onChanged: (String? newValue) {
+          setState(() {
+            dropdownValue = newValue!;
+          });
+        },
+
+        // Array list of items
+        items: items.map<DropdownMenuItem<String>>((String items) {
+          return DropdownMenuItem<String>(
+            value: items,
+            child: Text(items),
+          );
+        }).toList(),
+        // After selecting the desired option,it will
+        // change button value to selected value
+      ),
+    ),
+    formItemsDesign(Text(
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 30),
+        "Total de es: ${total}")),
   ]);
 }
+
+setState(Null Function() param0) {}
