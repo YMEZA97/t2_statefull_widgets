c7a701a3722a85b7df27c0f3d4107f7cdd2c06c5
YMEZA97 <yasalberto@hotmail.com>
2024-08-02 23:47:50 -0600

    T2 Main init - Update 2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,52 +1,74 @@
-
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
- 
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+TextEditingController n1 = TextEditingController();
+TextEditingController n2 = TextEditingController();
+TextEditingController sumatoria = TextEditingController();
+TextEditingController total = TextEditingController();
+
+GlobalKey<FormState> keyForm = GlobalKey();
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar (
+      appBar: AppBar(
         centerTitle: true,
         title: const Text('Operaciones Aritmeticas'),
       ),
-       
-       body: const Center(
-        child: TextFieldSample(),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(20.0),
+          child: Form(
+            key: keyForm,
+            child: formUI(),
+          ),
         ),
-
-
-   floatingActionButton: FloatingActionButton(
-    ///  onPressed: () => setState(() => _count++),
-        tooltip: 'Increment Counter',
-        onPressed: () {  },
-        child: const Icon(Icons.play_arrow),
-        
       ),
-
-
+      floatingActionButton: FloatingActionButton(
+        ///  onPressed: () => setState(() => _count++),
+        tooltip: 'Increment Counter',
+        onPressed: () {},
+        child: const Icon(Icons.play_arrow),
+      ),
     );
   }
 }
 
+formItemsDesign(icon, item) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 7),
+    child: Card(child: ListTile(leading: Icon(icon), title: item)),
+  );
+}
 
-class TextFieldSample extends StatelessWidget {
-  const TextFieldSample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(
-      width: 250,
-      child: TextField(
-        
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Password',
-        ),
+Widget formUI() {
+  return Column(
+    children: <Widget>[
+      formItemsDesign(TextFormField(
+        controller: n1,
+        keyboardType: const TextInputType.numberWithOptions(decimal: true),
+        decoration: const InputDecoration(
+            border: OutlineInputBorder(), 
+            labelText: 'Ingrese un nuemero'),
+          validator:,
       ),
-    );
-  }
+      formItemsDesign(
+        TextFormField(
+          controller: n2,
+          keyboardType: const TextInputType.numberWithOptions(decimal: true),
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(), 
+              labelText: 'Ingrese un nuemero'),
+       validator:,
+        )
+      )
+    ]
+  );
 }
